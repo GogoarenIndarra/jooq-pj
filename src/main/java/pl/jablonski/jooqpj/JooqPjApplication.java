dd41af96bea4f47e9a3c0fbc64530810253c0a71
jablonski.pio <piotr.jablonski@bcf-software.com>
2022-10-31 10:05:45 +0100

    Init commit

---
+++ src/main/java/pl/jablonski/jooqpj/JooqPjApplication.java
@@ -0,0 +1,13 @@
+package pl.jablonski.jooqpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooqPjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jablonski/jooqpj/JooqPjApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jablonski.jooqpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
